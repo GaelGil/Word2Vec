b03edddb5472c6c08f25b5ebb6c60eca2c6b21ca
Gael Gil <gaelgilbello27@gmail.com>
2026-02-03 16:10:26 -0800

    implementing model

--- src/model.rs
+++ src/model.rs
@@ -1,28 +1,27 @@
 use candle_core::{Result, Tensor};
 use candle_nn::{Embedding, Linear, Module, VarBuilder};
 
-pub struct MyModel {
+pub struct Model {
     embeddings: Embedding,
     linear1: Linear,
     linear2: Linear,
 }
 
-impl MyModel {
-    pub fn new(vb: VarBuilder) -> Result<Self> {
-        let embeddings = candle_nn::embedding(100, 100, vb.pp("embeddings"))?;
-        let linear1 = candle_nn::linear(100, 3, vb.pp("linear1"))?;
-        let linear2 = candle_nn::linear(3, 100, vb.pp("linear2"))?;
+impl Model {
+    pub fn new(vocab_size: usize, window_size: usize, vb: VarBuilder) -> Result<Self> {
+        let embeddings = candle_nn::embedding(vocab_size, 100, vb.pp("embeddings"))?;
+        let linear1 = candle_nn::linear(100, window_size, vb.pp("linear1"))?;
+        let linear2 = candle_nn::linear(window_size, vocab_size, vb.pp("linear2"))?;
         Ok(Self {
             embeddings,
             linear1,
             linear2,
         })
     }
+}
 
-    pub fn forward(&self, x: &Tensor) -> Result<Tensor> {
-        // let x = self.linear1.forward(x)?;
-        // let x = x.relu()?;
-        // self.linear2.forward(&x)
+impl Module for Model {
+    fn forward(&self, x: &Tensor) -> Result<Tensor> {
         let x = self.embeddings.forward(x)?;
         let x = self.linear1.forward(&x)?;
         self.linear2.forward(&x)
